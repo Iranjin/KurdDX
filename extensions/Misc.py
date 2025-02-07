fcb688eccbda7fd08633b2ebf03867d4df9a13f8
Iranjin <iranjin262@gmail.com>
2025-02-07 21:30:00 +0900

    Create Misc.py

---
+++ extensions/Misc.py
@@ -0,0 +1,20 @@
+import logging
+import io
+
+from discord.ext import commands
+
+from utils.exceptions import *
+from kurd_dx import KurdDX
+from base_cog import BaseCog
+
+
+class Misc_EXT(BaseCog):
+    @commands.hybrid_command(name="ping", description="Returns Pong!")
+    async def ping(self, ctx: commands.Context):
+        raw_ping = self.bot.latency
+        ping = round(raw_ping * 1000)
+        await ctx.send(f"Pong! `{ping}`")
+
+
+async def setup(bot: KurdDX):
+    await bot.add_cog(Misc_EXT(bot))
